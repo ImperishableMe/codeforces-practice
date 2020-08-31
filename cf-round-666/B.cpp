fbe80fe2b6be3f55d7fb99fc746db2ad01180a28
Bishwajit-Bhattacharjee <bishwajit0020@gmail.com>
2020-08-31 21:27:30 +0600

    Round 666 A-C, the rest are not beautiful

---
+++ cf-round-666/B.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+using namespace std;
+using namespace __gnu_pbds;
+
+double INF = 1e100;
+double EPS = 1e-12;
+typedef long long int ll;
+
+typedef pair < int,int > PII;
+typedef pair < ll,ll > PLL;
+
+typedef tree<
+int,
+null_type,
+less<int>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+#define F first
+#define S second
+#define all(v) (v).begin(),(v).end()
+
+ostream& operator<<(ostream & os, PLL h){
+	return os << "( " << h.F << ", " << h.S << " )" << endl;
+
+}
+PLL operator+ (PLL a, ll x)     {return {a.F + x, a.S + x} ;}
+PLL operator- (PLL a, ll x)     {return {a.F - x, a.S - x} ;}
+PLL operator* (PLL a, ll x)     {return {a.F * x, a.S * x} ;}
+PLL operator+(PLL x, PLL y) { return {x.F + y.F,x.S + y.S} ;}
+PLL operator-(PLL x,PLL y) { return {x.F - y.F, x.S - y.S} ;}
+PLL operator*(PLL x,PLL y) { return {x.F * y.F , x.S * y.S} ;}
+PLL operator%(PLL x,PLL y) { return {x.F % y.F, x.S % y.S} ;}
+
+
+ll const MOD = 1e9 + 7;
+
+ll bigmod(ll a, ll b){
+	if(!b) return 1;
+	ll x  = bigmod(a,b/2);
+
+	x = (x * x)%MOD;
+	if(b&1)
+		x = (x * a) %MOD;
+	return x;
+}
+
+vector < ll > primes;
+vector < bool > marks;
+
+void sieve(int n)
+{
+    marks.resize(n+10,0);
+	marks[1] = 1;
+	for(int i = 2; i < n; i++){
+		if(!marks[i]){
+			for(int j = 2*i; j < n; j += i){
+				marks[j] = 1;
+			}
+			primes.push_back(i);
+		}
+	}
+}
+ll const UP = 1e16;
+
+bool check(ll now, ll mul){
+	return (1.0* now * mul < UP);
+}
+int main(){
+
+	ios::sync_with_stdio(false); cin.tie(0);
+	int n;
+	cin >> n;
+	vector<ll> v(n);
+	for(ll &x : v) cin >> x;
+	sort(all(v));
+
+	ll ans= 1e18;
+	for(ll i = 1; ; i++){
+		ll can = 0, mul = 1;
+		bool done = 0;
+		for(int j = 0; j < v.size(); j++){
+			ll x = v[j] ;
+			can += abs(x-mul);
+			if(j + 1 == v.size()) ans = min(ans, can);
+			if(check(mul, i)) mul *= i;
+			else {
+				done = 1; break;
+			}
+			// cout << i << " " << can << " " << ans << endl;
+		}
+		if(can > UP) break;
+		if(done) break;
+	}
+	cout << ans << endl;
+
+	return 0;
+}
